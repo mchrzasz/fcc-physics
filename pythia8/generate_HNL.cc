43216f3f40bb784e1c48515c60ab90b05d38f9f0
Marcin Chrzaszcz <mchrzasz@cern.ch>
2016-04-10 00:02:15 +0200

    large chunk of code fir HNL calculations

--- pythia8/generate_HNL.cc
+++ pythia8/generate_HNL.cc
@@ -12,6 +12,8 @@
 // STL
 #include <iostream>
 #include <vector>
+#include <map>
+#include <unordered_map>
 
 // albers specific includes
 #include "podio/EventStore.h"
@@ -27,9 +29,174 @@
 
 // root stuff:
 #include "TDatabasePDG.h"               // for TDatabasePDG
+#include "TParticlePDG.h"
+
+
+namespace CKM
+{
+      // a bit old fit, take the new one in the future
+  static const double  Vud = 0.9742;
+  static const double Vus = 0.2252;
+  static const double    Vub = 4.2e-03;
+  static const double    Vcd = 0.23;
+  static const double    Vcs = 1.;
+  static const double    Vcb = 4.1e-02;
+  static const double    Vtd = 8.4e-03;
+  static const double    Vts = 4.3e-02;
+  static const double    Vtb = 0.89;
+
+
+}
+namespace constants
+{
+  static const double  GF=1.166379e-05;//GeV
+  static const double  s2thetaw=0.23126;
+  static const double pi=3.1415;
+
+}
+
+double decayConstant(int i)
+{
+  std::map<int,double> mymap;
+  mymap[211]=0.1307;
+  mymap[111]=0.130;
+  mymap[113]=0.102;//rho0
+  mymap[213]=0.102;  //rho+
+  mymap[221]=1.2*0.130;  //eta
+  mymap[331]=-0.45*0.130;  //eta(958)
+
+  return mymap[i];
+
+}
+double CKMelemSq(int a)
+{
+
+  std::unordered_map<int, double> CKMelemSq = {{211,CKM::Vud*CKM::Vud}, //pi
+                                               {113,CKM::Vud*CKM::Vud},  //rho
+                                               {321,CKM::Vus*CKM::Vus}}  ; //K
+  return CKMelemSq[a];
+}
+double CKMelemSq(int a)
+{
+  std::unordered_map<int, double> CKMelemSq =
+
+
+  return CKMelemSq[a];
+
+}
+
+
+
+
+
+
+class couplings
+{
+public:
+  double U2e, U2mu, U2tau;
+  double u[3];
+  couplings()
+  {
+    U2e=0.;
+    U2mu=0.;
+    U2tau=0.;
+    u[0]= U2e;
+    u[1]= U2mu;
+    u[2]=U2tau;
+  }
+  couplings(double u2e, double u2mu, double u2tau)
+  {
+    U2e=u2e;
+    U2mu=u2mu;
+    U2tau=u2tau;
+    u[0]= U2e;
+    u[1]= U2mu;
+    u[2]=U2tau;
+  };
+  double &operator[](int i)
+  {
+    return u[i];
+  }
+
+
+};
+
+
+
+
+
 
 
 using namespace std;
+
+
+
+
+class HNL
+{
+  // class thar provides calculations of Branching fractions and width
+  // for different couplings and masses of HNL
+  // M.Chrzaszcz, 09.04.2015
+  HNL(couplings u, double Mass)
+  {
+    U2=u;
+    U=couplings(sqrt(u.U2e), sqrt(u.U2mu), sqrt(u.U2tau));
+    pdgBase = TDatabasePDG::Instance();
+    m=Mass;
+
+
+  };
+  double Width_H_l(int h, int l)
+  {
+    //###################################################################
+    // returns the decay width of HNL to charge meson and lepton
+    // is the hadron int, int is lepton
+    //###################################################################
+    double alpha;
+    if(abs(l)==11) alpha=1;
+    if(abs(l)==13) alpha=2;
+    if(abs(l)==15) alpha=3;
+
+    TParticlePDG *tPartH = pdgBase->GetParticle(h);
+    double h_m=tPartH->Mass();
+    TParticlePDG *tPartl = pdgBase->GetParticle(l);
+    double l_m=tPartl->Mass();
+
+    if(m<h_m+l_m) return 0.; // no decay
+
+    double width = (abs(U2[alpha-1])/(16.*constants::pi))*(constants::GF * constants::GF)*(decayConstant(h)*decayConstant(h));
+    width = width*(pow(m,3.))*CKMelemSq(h);
+
+
+
+
+
+
+
+  }
+
+
+
+
+private:
+
+  couplings U2;
+  couplings U;
+  double m;
+
+  TDatabasePDG* pdgBase ;//= TDatabasePDG::Instance();
+
+
+
+
+};
+
+
+
+
+
+
+
 
 int main(int argc, char** argv) {
   if( argc != 2) {
@@ -67,23 +234,23 @@ int main(int argc, char** argv) {
   writer.registerForWrite<fcc::GenJetCollection>("GenJet");
   // declaring new particles:
   TDatabasePDG* pdgBase = TDatabasePDG::Instance();
-  
-  double m=1.0;  
+
+  double m=1.0;
   double g=1.0;
   pdgBase->AddParticle("N2","HNL", m, false, g, 0., "N2", 9900015);
-    
+
   // Generator. Process selection. LHC initialization. Histogram.
   Pythia8::Pythia pythia;
   pythia.readString("9900015:new = N2 N2 2 0 0 1.0 0.0 0.0 0.0 1.  0   1   0   1   0");
-  
 
-  
+
+
   pythia.readFile(card_file);
   //pythia.readString("23:oneChannel =  1 1 0 9900015 16");
   //pythia.SetParameters("9900015:oneChannel =  1 1 0 13 -13");
-  
-  
-    
+
+
+
   //  std::cout<<"Listing Particle changes: "<<std::endl;
   //pythia.particleData.listAll();
   //std::cout<<"Done listing"<<std::endl;
@@ -91,19 +258,19 @@ int main(int argc, char** argv) {
   //  pythia.particleData.readXML("/afs/cern.ch/user/m/mchrzasz/FCC/fcc-physics/pythia8/ParticleData_HNL.xml");
   //std::cout<<"Changes in particles: "<<std::endl;
   //pythia.particleData.listChanged();
-  
+
   //  pythia.particleData.listAll();
-  
+
   //pythia.readFile("/afs/cern.ch/user/m/mchrzasz/FCC/fcc-physics/pythia8/ParticleData_HNL_short.xml");
-  
-  
-  
-   
-  
+
+
+
+
+
   pythia.init();
-  
+
   unsigned nevents = pythia.mode("Main:numberOfEvents");
-  
+
   // Interface for conversion from Pythia8::Event to HepMC event.
   HepMC::Pythia8ToHepMC ToHepMC;
   Pythia8::ParticleData PDG;
